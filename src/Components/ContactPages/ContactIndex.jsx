6413268d22864b9489668da5c9f75b5cb3a5c601
david <davidtucu@hotmail.com>
2023-12-10 10:26:15 -0300

    ui de algunas pantallas

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -7,6 +7,34 @@ import FavoriteContacts from "./FavoriteContact";
 import GeneralContacts from "./GeneralContact";
 import RemoveAllContact from "./RemoveAllContact";
 class ContactIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          contactList: [
+            {
+              id: 1,
+              name: "Ben Parker",
+              phone: "666-666-7770",
+              email: "ben@dotnetmastery.com",
+              isFavorite: false,
+            },
+            {
+              id: 2,
+              name: "Kathy Patrick",
+              phone: "111-222-0000",
+              email: "kathy@dotnetmastery.com",
+              isFavorite: true,
+            },
+            {
+              id: 3,
+              name: "Paul Show",
+              phone: "999-222-1111",
+              email: "paul@dotnetmastery.com",
+              isFavorite: true,
+            },
+          ],
+        };
+      }
     render() {
         return (
             <div>
@@ -23,10 +51,18 @@ class ContactIndex extends React.Component {
                         <AddContact />
                     </div>
                     <div className="row py-2">
-                        <FavoriteContacts />
+                    <FavoriteContacts
+                            contacts={this.state.contactList.filter(
+                            (u) => u.isFavorite === true
+                            )}
+                        />
                     </div>
                     <div className="row py-2">
-                        <GeneralContacts />
+                    <GeneralContacts
+                            contacts={this.state.contactList.filter(
+                            (u) => u.isFavorite === false
+                            )}
+                        />
                     </div>
                 </div>
                 </div>
